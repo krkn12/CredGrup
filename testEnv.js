2c3b6e7b9ed05b0deacf86d4577c3bde758c2c7c
@SilvaJosias <72473790+krkn12@users.noreply.github.com>
2025-03-21 12:04:17 -0300

    Remove arquivos desnecessários: 'generateCert.js' e 'testEnv.js'.

--- testEnv.js
+++
@@ -1,5 +0,0 @@
-require('dotenv').config({ path: './.env' });
-console.log('PORT:', process.env.PORT);
-console.log('JWT_SECRET:', process.env.JWT_SECRET);
-console.log('ARBISCAN_API_KEY:', process.env.ARBISCAN_API_KEY);
-console.log('MONGODB_URI:', process.env.MONGODB_URI);
