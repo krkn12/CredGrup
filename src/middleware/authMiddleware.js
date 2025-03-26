66a433e958dc9cb9c72feb0f0b1a655a36a3bf80
@SilvaJosias <72473790+krkn12@users.noreply.github.com>
2025-03-26 00:29:27 -0300

    fix: Adiciona logs de depuração no middleware de autenticação
    Este commit inclui logs para exibir os cabeçalhos recebidos e o token no middleware de autenticação. Essas adições visam melhorar a rastreabilidade e facilitar a depuração de problemas relacionados à autenticação de usuários, garantindo maior segurança e integridade dos dados na aplicação.

--- src/middleware/authMiddleware.js
+++ src/middleware/authMiddleware.js
@@ -1,22 +1,23 @@
 const jwt = require('jsonwebtoken');
 
 const authMiddleware = (req, res, next) => {
-    try {
-        const authHeader = req.header('Authorization');
-        if (!authHeader || !authHeader.startsWith('Bearer ')) {
-            return res.status(401).send({ error: 'Token não fornecido' });
-        }
+  try {
+    const authHeader = req.header('Authorization');
+    console.log('Cabeçalhos recebidos no authMiddleware:', req.headers);
+    console.log('Token recebido no authMiddleware:', authHeader);
 
-        const token = authHeader.replace('Bearer ', '');
-        console.log('Token recebido no authMiddleware:', token);
-
-        const decoded = jwt.verify(token, process.env.JWT_SECRET);
-        req.user = decoded;
-        next();
-    } catch (error) {
-        console.error('Erro no authMiddleware:', error.message);
-        res.status(401).send({ error: 'Autenticação falhou' });
+    if (!authHeader || !authHeader.startsWith('Bearer ')) {
+      return res.status(401).send({ error: 'Token não fornecido' });
     }
+
+    const token = authHeader.replace('Bearer ', '');
+    const decoded = jwt.verify(token, process.env.JWT_SECRET);
+    req.user = decoded;
+    next();
+  } catch (error) {
+    console.error('Erro no authMiddleware:', error.message);
+    res.status(401).send({ error: 'Autenticação falhou' });
+  }
 };
 
 module.exports = authMiddleware;
